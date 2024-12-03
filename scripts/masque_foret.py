ed029c711615542d4644ed7fea21a0ce5dd9f4ca
Lucas Lima <caslumali@gmail.com>
2024-12-03 13:27:52 +0100

    ajout no data dans masque forêt

--- scripts/masque_foret.py
+++ scripts/masque_foret.py
@@ -36,6 +36,8 @@ sptial_resolution, xmin, ymin, xmax, ymax = get_reprojected_raster_properties(
 # commande cmd pour le masque avec compression
 cmd_pattern = (
     "gdal_rasterize -burn 1 "  # foret = 1
+    "-init 0 "
+    "-a_nodata 99 "  # defining the nodata
     "-tr {sptial_resolution} {sptial_resolution} "
     "-te {xmin} {ymin} {xmax} {ymax} -ot Byte -of GTiff "
     "-co COMPRESS=LZW "
