13ad515a2339fa7f6749b5b2a4820c5fca3d319f
Lucas Lima <caslumali@gmail.com>
2024-12-04 20:41:39 +0100

    fin commit

--- scripts/masque_foret.py
+++ scripts/masque_foret.py
@@ -12,7 +12,6 @@ from my_function import create_forest_mask, find_raster_bands, log_error_and_rai
 
 # Chemins d'accès relatifs
 mask_vector = "groupe_9/results/data/sample/Sample_BD_foret_T31TCJ.shp"
-<< << << < HEAD
 input_folder = "data/images/"
 band_prefix = "FRE_B2"
 
@@ -26,10 +25,6 @@ else:
     reference_image = reference_images[0]
 
 clip_vector = "data/project/emprise_etude.shp"
-== == == =
-reference_image = "data/images/SENTINEL2A_20220209-105857-811_L2A_T31TCJ_C_V3-0/SENTINEL2A_20220209-105857-811_L2A_T31TCJ_C_V3-0_FRE_B2.tif"
-clip_vector = "data/vecteurs/emprise_etude.shp"
->>>>>> > 484ef419a5aa257a1b010550f25fcb8bb3840990
 output_image = "groupe_9/results/data/img_pretraitees/mask_forest.tif"
 
 # Créer le masque
