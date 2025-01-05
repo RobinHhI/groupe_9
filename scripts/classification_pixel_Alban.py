900cc6afd3424cc48d0219e7e2edc12e8d453d52
Alban <alban.dumont-1731@netcourrier.com>
2025-01-05 15:33:40 +0100

    # Classification pixel Ok, log et remodelage à faire

--- scripts/classification_pixel_Alban.py
+++ scripts/classification_pixel_Alban.py
@@ -15,16 +15,13 @@ Créé le 28 décembre 2024
 import os
 import sys
 sys.path.append('libsigma')
-import re  # pour utiliser fonction regex
-import time  # Pour calculer le temp de traitement
-import logging
 import numpy as np
 from sklearn import tree
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import confusion_matrix, classification_report, \
     accuracy_score
 from sklearn.ensemble import RandomForestClassifier as RF
-import geopandas as gpd
+import pandas as pd
 from datetime import datetime
 
 from my_function_alban import create_raster_sampleimage
@@ -32,17 +29,27 @@ from my_function_alban import create_raster_sampleimage
 # personal librairies
 import classification as cla
 import read_and_write as rw
-import plots
-"""
-from my_function import (find_raster_bands, reproject_raster, clip_raster_to_extent,
-                         resample_raster, apply_mask, merge_rasters,
-                         calculate_ndvi_from_processed_bands)
-"""
-
-# Démarrage du chronomètre de traitement total
-total_start_time = time.time()
 
 
+def report_from_dict_to_df(dict_report):
+
+
+    # convert report into dataframe
+    report_df = pd.DataFrame.from_dict(dict_report)
+
+    # drop unnecessary rows and columns
+    try :
+        report_df = report_df.drop(['accuracy', 'macro avg', 'weighted avg'], axis=1)
+    except KeyError:
+        print(dict_report)
+        report_df = report_df.drop(['micro avg', 'macro avg', 'weighted avg'], axis=1)
+
+    report_df = report_df.drop(['support'], axis=0)
+
+    return report_df
+
+print(" Debut : " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+'2018-07-17 22:54:25'
 # 1 --- define parameters
 # inputs
 sample_folder = "groupe_9/results/data/sample"
@@ -51,71 +58,109 @@ sample_filename = os.path.join(sample_folder, "Sample_BD_foret_T31TCJ.shp")
 image_reference = os.path.join(pretraitees_folder, "masque_foret.tif")
 image_filename = os.path.join(pretraitees_folder, "Serie_temp_S2_ndvi.tif")
 raster_sample_filename = os.path.join(sample_folder, "Sample_BD_foret_T31TCJ.tif")
-
-
-# Sample parameters
-test_size = 0.5
+raster_sample_id_filename = os.path.join(sample_folder, "Sample_id_BD_foret_T31TCJ.tif")
 
 # outputs
 out_classif_folder = 'groupe_9/results/data/classif'
-classif_filename = 'carte_essences_echelle_pixel.tif'
-out_classif = os.path.join(out_classif_folder, classif_filename)
+out_classif = os.path.join(out_classif_folder, 'carte_essences_echelle_pixel.tif')
 out_matrix = os.path.join(out_classif_folder, 'ma_matrice.png')
 out_qualite = os.path.join(out_classif_folder, 'mes_qualites.png')
+out_std_dev_mean = os.path.join(out_classif_folder, 'Std_Dev_and_Mean.png')
+out_std_deviation_pkl =  os.path.join(out_classif_folder, 'std_deviation.pkl')
+out_mean_pkl =  os.path.join(out_classif_folder, 'out_mean.pkl')
+out_std_deviation_csv =  os.path.join(out_classif_folder, 'std_deviation.csv')
+out_mean_csv =  os.path.join(out_classif_folder, 'mean.csv')
 
+# Sample parameters
+nb_iter = 30
 
-# create_raster_sampleimage(sample_filename, image_reference, raster_sample_filename)
+print(" Création du raster Sample : " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+create_raster_sampleimage(sample_filename, image_reference, raster_sample_filename, "Code")
+print(" Création du raster Sample Id : " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+create_raster_sampleimage(sample_filename, image_reference, raster_sample_id_filename, "ID")
+
 
 # 2 --- extract samples
+print(" Obtention du Sample X, Y, t  : " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 X, Y, t = cla.get_samples_from_roi(image_filename, raster_sample_filename)
+print(" Obtention des groupes : " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+_, groups, _ = cla.get_samples_from_roi(image_filename, raster_sample_id_filename)
 
+# Valeurs à remplacer
+values_to_replace = [15, 16, 26,27, 28 ,29]
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size)
+# Remplacement
+Y[np.isin(Y, values_to_replace)] = 0
 
-Y_train = np.ravel(Y_train)
-print(Y_train[0:10])
+list_cm = []
+list_accuracy = []
+list_report = []
+groups = np.squeeze(groups)
 
-n_estimators=40
-max_depth =	50
-oob_score = True
-max_samples = 0.75
-class_weight = "balanced"
+# Iter on stratified K fold
+print(" Création Kfold Statifié : " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+kf = StratifiedKFold(n_splits=nb_iter)
+for train, test in kf.split(X, Y):
+    print(" Entrainement sur tous les Kfolds : " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    X_train, X_test = X[train], X[test]
+    Y_train, Y_test = Y[train], Y[test]
 
-# 3 --- Train
-#clf = SVC(cache_size=6000)
-clf = RF(n_estimators=n_estimators, max_depth=max_depth, oob_score=oob_score, max_samples=max_samples, class_weight=class_weight)
-#clf = tree.DecisionTreeClassifier(max_leaf_nodes=10)
-clf.fit(X_train, Y_train)
+    # 3 --- Train
+    #clf = SVC(cache_size=6000)
+    clf = tree.DecisionTreeClassifier()
+    clf.fit(X_train, Y_train)
 
-# 4 --- Test
-Y_predict = clf.predict(X_test)
+    # 4 --- Test
+    Y_predict = clf.predict(X_test)
 
-#Y_test = np.squeeze(Y_test)
-#print(Y_test[0:5])
+    Y_predict[Y_predict == 4] = 3
+    Y_test[Y_test == 4] = 3
+    # compute quality
+    list_cm.append(confusion_matrix(Y_test, Y_predict))
+    list_accuracy.append(accuracy_score(Y_test, Y_predict))
+    report = classification_report(Y_test, Y_predict,
+                                   labels=np.unique(Y_predict), output_dict=True)
 
-# Convert it into two dimensions array
-Y_predict = np.atleast_2d(Y_predict)
+    # store them
+    list_report.append(report_from_dict_to_df(report))
 
-# Inverse the dimensions
-Y_predict = Y_predict.T
+print(" Sauvegarde des résultats : " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+# compute mean of cm
+array_cm = np.array(list_cm)
+cm_mean = array_cm.mean(axis=0)
 
-print(np.unique(Y_predict))
-print(np.unique(Y))
+# compute mean and std of overall accuracy
+array_accuracy = np.array(list_accuracy)
+mean_accuracy = array_accuracy.mean()
+std_accuracy = array_accuracy.std()
 
+# compute mean and std of classification report
+array_report = np.array(list_report)
+mean_report = array_report.mean(axis=0)
+std_report = array_report.std(axis=0)
+a_report = list_report[0]
 
-# compute quality
-cm = confusion_matrix(Y_test, Y_predict)
-report = classification_report(Y_test, Y_predict, labels=np.unique(Y_predict), output_dict=True)
-accuracy = accuracy_score(Y_test, Y_predict)
+mean_df_report = pd.DataFrame(mean_report, index=a_report.index,
+                              columns=a_report.columns)
+std_df_report = pd.DataFrame(std_report, index=a_report.index,
+                             columns=a_report.columns)
 
-# display and save quality
-plots.plot_cm(cm, np.unique(Y_predict), out_filename=out_matrix)
-plots.plot_class_quality(report, accuracy, out_filename=out_qualite)
+# sauvegarde les résultats en format pickle
+# permet de sauvegarder le type de données de chaque colonne 
+mean_df_report.to_pickle(out_mean_pkl)
+std_df_report.to_pickle(out_std_deviation_pkl)
+# et en csv, pour être plus facilement manipulable en dehors de python
+mean_df_report.to_csv(out_mean_csv)
+std_df_report.to_csv(out_std_deviation_csv)
+# sauvegarde avec en image
+plots.plot_mean_class_quality(list_report, list_accuracy, out_std_dev_mean)
 
+print(" Prédiction sur toute la zone : " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 Y_predict = clf.predict(X)
 print(np.unique(Y_predict))
 
 # reshape
+print(" Sauvegarde de la nouvelle Carte : " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
 ds = rw.open_image(image_filename)
 nb_row, nb_col, _ = rw.get_image_dimension(ds)
 
@@ -123,7 +168,6 @@ img = np.zeros((nb_row, nb_col, 1), dtype='uint8')
 img[t[0], t[1], 0] = Y_predict
 
 # write image
-ds = rw.open_image(image_filename)
-rw.write_image(out_classif, img, data_set=ds, gdal_dtype=None,
-            transform=None, projection=None, driver_name=None,
-            nb_col=None, nb_ligne=None, nb_band=1)
+rw.write_image(out_classif, img, data_set=ds, nb_band=1)
+print(" Fin : " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
--- scripts/my_function_alban.py
+++ scripts/my_function_alban.py
@@ -17,6 +17,8 @@ import traceback
 import logging
 import numpy as np
 from osgeo import gdal
+from osgeo import ogr
+
 import geopandas as gpd
 
 # fonction déjà existante dans my_function.py - à ne pas copier
@@ -94,7 +96,7 @@ def get_raster_properties(dataset):
 #fin des fonctions déjà existantes
 
 
-def create_raster_sampleimage(sample_vector, reference_raster, output_path):
+def create_raster_sampleimage(sample_vector, reference_raster, output_path, attribute):
     """
     Crée un raster forêt pour la classification   :
         - .
@@ -127,12 +129,6 @@ def create_raster_sampleimage(sample_vector, reference_raster, output_path):
         pixel_width, pixel_height, xmin, ymin, xmax, ymax, crs = get_raster_properties(
             reference_ds)
 
-        # Alignement des bornes pour correspondre à la résolution
-        xmin_aligned = xmin - (xmin % pixel_width)
-        ymin_aligned = ymin - (ymin % pixel_height)
-        xmax_aligned = xmax + (pixel_width - (xmax % pixel_width))
-        ymax_aligned = ymax + (pixel_height - (ymax % pixel_height))
-
         # Calcul du nombre de pixels en directions x et y
         x_pixels = int((xmax - xmin) / pixel_width)
         y_pixels = int((ymax - ymin) / pixel_height)
@@ -140,8 +136,13 @@ def create_raster_sampleimage(sample_vector, reference_raster, output_path):
         # Étape 2 : Création d'un raster en mémoire pour rasteriser le masque vecteur
         logger.info("Rasterisation du sample forêt...")
         mem_driver = gdal.GetDriverByName('MEM')
-        out_raster = mem_driver.Create(
-            '', x_pixels, y_pixels, 1, gdal.GDT_Byte)
+
+        if (attribute == "ID") :
+            out_raster = mem_driver.Create(
+                '', x_pixels, y_pixels, 1, gdal.GDT_UInt16)
+        else :    
+            out_raster = mem_driver.Create(
+                '', x_pixels, y_pixels, 1, gdal.GDT_Byte)
         out_raster.SetGeoTransform(
             (xmin, pixel_width, 0, ymax, 0, -pixel_height))
         out_raster.SetProjection(crs)
@@ -155,12 +156,102 @@ def create_raster_sampleimage(sample_vector, reference_raster, output_path):
         # Ouvrir le masque vecteur
         vector_ds = gdal.OpenEx(sample_vector, gdal.OF_VECTOR)
         if vector_ds is None:
-            log_error_and_raise(
+            log_error_and_raise( 
                 f"Impossible d'ouvrir le fichier vecteur : {sample_vector}")
+        vector_layer = vector_ds.GetLayer()
+        if (attribute == "ID") :
+            
+            sample_folder = "groupe_9/results/data/sample"
+            filename_tmp = os.path.join(sample_folder, "tmp_id.shp")
+                                          
+            # Vérifier si le fichier destination existe déjà
+            if os.path.exists(filename_tmp):
+                os.remove(filename_tmp)
 
+            # Créer un nouveau fichier de destination
+            driver = ogr.GetDriverByName("ESRI Shapefile")
+            destination_dataset = driver.CreateDataSource(filename_tmp)
+            if destination_dataset is None:
+                log_error_and_raise("Impossible de créer le fichier tmp.")
+                
+            # Créer une nouvelle couche dans le dataset de destination
+            destination_layer = destination_dataset.CreateLayer(
+                vector_layer.GetName(), vector_layer.GetSpatialRef(), 
+                geom_type=vector_layer.GetGeomType(), options=["ENCODING=UTF-8"])
+
+            # Copier les champs de la couche source
+            source_layer_def = vector_layer.GetLayerDefn()
+            for i in range(source_layer_def.GetFieldCount()):
+                field_def = source_layer_def.GetFieldDefn(i)
+                destination_layer.CreateField(field_def)
+
+            # Parcourir les entités de la couche source
+            for feature in vector_layer:
+                # Créer une nouvelle entité dans la couche de destination
+                new_feature = ogr.Feature(destination_layer.GetLayerDefn())
+
+                # Copier les valeurs des champs
+                FieldCount = source_layer_def.GetFieldCount()
+                for i in range(FieldCount):
+                    Nameref = source_layer_def.GetFieldDefn(i).GetNameRef()
+                    Field = feature.GetField(i)
+                    new_feature.SetField(Nameref, Field)
+
+                # Copier la géométrie
+                geom = feature.GetGeometryRef()
+                new_feature.SetGeometry(geom.Clone())
+
+                # Ajouter la nouvelle entité à la couche de destination
+                destination_layer.CreateFeature(new_feature)
+
+                # Nettoyer
+                new_feature = None
+            
+            # Définir le nom des champs
+            new_field_name = "temp_" + attribute
+
+            # Obtenir la définition des champs
+            layer_def = destination_layer.GetLayerDefn()
+
+            # Vérifier si le champ à modifier existe
+            old_field_index = layer_def.GetFieldIndex(attribute)
+            if old_field_index == -1:
+                log_error_and_raise( f"Erreur : le champ '{attribute}' n'existe pas.")
+
+            # Ajouter un nouveau champ temporaire avec le type souhaité
+            new_field = ogr.FieldDefn(new_field_name, ogr.OFTInteger)  # Exemple : Integer
+            destination_layer.CreateField(new_field)
+
+            # Copier les données de l'ancien champ vers le nouveau champ
+            for feature in destination_layer:
+                FID = feature.GetFID()
+                feature.SetField(new_field_name, FID + 1)  # Conversion en entier
+                destination_layer.SetFeature(feature)
+
+            # Supprimer l'ancien champ
+            destination_layer.DeleteField(old_field_index)
+
+            # Renommer le nouveau champ pour qu'il remplace l'ancien
+            layer_def = destination_layer.GetLayerDefn()  # Rafraîchir la définition
+            src_fdef = layer_def.GetFieldDefn(layer_def.GetFieldIndex(new_field_name))
+            fdef = ogr.FieldDefn(src_fdef.GetName(), src_fdef.GetType())
+            fdef.SetWidth(src_fdef.GetWidth())
+            fdef.SetPrecision(src_fdef.GetPrecision())
+            fdef.SetName(attribute)
+            fdef.SetType(ogr.OFTInteger)
+            new_field_index = layer_def.GetFieldIndex(new_field_name)
+            
+            destination_layer.AlterFieldDefn(new_field_index, fdef, ogr.ALTER_NAME_FLAG)
+            
+            
         # Rasteriser le sample forêt avec la valeur de l'attribut Code
-        err = gdal.RasterizeLayer(
-            out_raster, [1], vector_ds.GetLayer(), options=["ATTRIBUTE=Code"])
+        if (attribute == 'ID') : 
+            err = gdal.RasterizeLayer(
+                out_raster, [1], destination_layer, options=["ATTRIBUTE={}".format(attribute)])
+            destination_dataset = None
+        else :
+            err = gdal.RasterizeLayer(
+                out_raster, [1], vector_layer, options=["ATTRIBUTE={}".format(attribute)])
         if err != 0:
             log_error_and_raise("Échec de la rasterisation.")
 
@@ -188,11 +279,9 @@ def create_raster_sampleimage(sample_vector, reference_raster, output_path):
         output_ds.FlushCache()
         output_ds = None
         out_raster = None
-        clipped_mask = None
 
         logger.info(f"Fiche sample forêt rasterisé sauvegardé : {output_path}")
 
     except Exception as e:
         log_error_and_raise(
             f"Erreur pendant la création du raster sample forêt : {e}")
-
